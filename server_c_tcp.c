e45446b650f7aaf436e935a15df4e2c206825a18
Karena Lai <karena_lai@ucsb.edu>
2025-10-23 19:08:04 -0400

    Add comments to server_c_tcp.c, fix temp size and irrelevant \r

--- server_c_tcp.c
+++ server_c_tcp.c
@@ -6,11 +6,13 @@
 #include <stdbool.h>
 #include <ctype.h> // for isdigit()
 
+// Print error message and exit
 void error(const char *msg){
     perror(msg);
     exit(0);
 }
 
+// Determine if a string contains only digits
 bool is_all_numbers(char* str){
     if (str == NULL || *str == '\0') {
         return false;
@@ -26,6 +28,7 @@ bool is_all_numbers(char* str){
     return true;
 }
 
+// Return sum of all digits in a string
 int get_sum(char* str){
     int result = 0;
     while (*str != '\0') {
@@ -48,6 +51,7 @@ int main(int argc, char *argv[]) {
     socklen_t client_len = sizeof(client_addr);
     char buffer[129];
 
+    // Socket setup
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) 
         error("ERROR opening socket");
@@ -60,37 +64,49 @@ int main(int argc, char *argv[]) {
     if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) 
         error("ERROR on binding");
 
+    // Listen for connections
     listen(server_fd, 5);
     client_len = sizeof(client_addr);
     
     while (1) {
+        // Establish connection with client
         client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-
+        
         if (client_fd < 0) {
             error("ERROR on accept");
             continue; 
         }
 
+        // Process client message
         ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
         if (bytes_received <= 0) {
             close(client_fd);
             continue; 
         }
+
+        // Ensure \0 at end
         buffer[bytes_received] = '\0';
-        buffer[strcspn(buffer, "\r\n")] = '\0';
+        buffer[strcspn(buffer, "\n")] = '\0';
+
         if (!is_all_numbers(buffer)) {
             memset(buffer, 0, sizeof(buffer));
             sprintf(buffer, "%s", "Sorry, cannot compute!\n");
             send(client_fd, buffer, strlen(buffer), 0);
         }
+
         else {
             int sum = 0;
+
+            // Ensure \0 at end
             buffer[bytes_received] = '\0';
-            buffer[strcspn(buffer, "\r\n")] = '\0';
+            buffer[strcspn(buffer, "\n")] = '\0';
+
             char result[129] = "";
+            
+            // Retrieve sum and store in final result
             do {
                 sum = get_sum(buffer);
-                char temp[32];
+                char temp[6];
                 sprintf(temp, "%d\n", sum);
                 strcat(result, temp);
                 sprintf(buffer, "%d", sum);
